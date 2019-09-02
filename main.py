77e3a053c8d8312ab6cc00adc731cf92f806ffd1
zaboevai <zaboevai@yandex.ru>
2019-09-02 14:54:23 +0300

    add foreign key

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from models import News, Post
+from models import News, Post, User
 from my_orm import DataBase
 
 db = DataBase()
 db.init_data_base()
 db.insert(News, ({'title': 'Заголовок 1', 'text': 'Новость 1'}, {'title': 'Заголовок 3', 'text': 'Новость 3'}))
-# db.insert(News, {'title': 'Заголовок 2', 'text': 'Новость 2'})
-# db.insert(News, {'title': 'Заголовок 3', 'text': 'Новость 3'})
-# db.select(News, {})
-# db.select(News, {'title': 'Заголовок 2;'})
+db.insert(News, {'title': 'Заголовок 2', 'text': 'Новость 2'})
+db.insert(News, {'title': 'Заголовок 3', 'text': 'Новость 3'})
 
-db.insert(Post, ({'title': 'Заголовок 3', 'text': 'Новость 3'}))
+db.select(News, )
+db.select(News, {'title': 'Заголовок 4'})
+
+db.insert(User, {'name': 'Admin'})
+
+db.select(User, {'id': '1'})
+
+db.insert(Post, {'user_id': '1', 'title': 'Заголовок 5', 'text': 'Новость 5'})
+db.insert(Post, {'user_id': '2', 'title': 'Заголовок 6', 'text': 'Новость 6'})
+
+db.select(Post, {'title': 'Заголовок 3'})
+
 # db.drop_table(News)
-
-# db = DataBase()
-n = News().get_columns()
-p = Post().get_columns()
--- models.py
+++ models.py
@@ -3,15 +3,13 @@ class Base:
     __table_name__ = ''
 
     def get_columns(self):
-        field_names = [f'{k} {" ".join(v)}' for k, v in self.__class__.__dict__.items() if not k.startswith('__')]
-        # print(field_names)
-        # print('SELECT %s FROM %s;' % (', '.join(field_names), self.__class__.__table_name__))
+        field_names = [f'{k if k != "key" else ""} {" ".join(v)}' for k, v in self.__class__.__dict__.items() if
+                       not k.startswith('__')]
         return field_names
 
 
 class News(Base):
     __table_name__ = 'news'
-
     id = Base.id
     title = ('char(256)', 'not null')
     text = ('char(256)', 'not null')
@@ -28,6 +26,6 @@ class Post(Base):
     __table_name__ = 'post'
 
     id = Base.id
-    # user_id = ('INTEGER', 'FOREIGN KEY', 'NOT NULL')
-    title = ('char(256)', 'null')
-    text = ('char(256)', 'null')
+    user_id = ('INTEGER', 'not null', 'REFERENCES user(id)')
+    title = ('char(256)', 'not null')
+    text = ('char(256)', 'not null')
